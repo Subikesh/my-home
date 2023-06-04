b4addd99237e954fa90b07a3ea464232ec9d3455
subikesh <subikeshps@gmail.com>
2023-06-04 16:27:46 +0530

    Used diff util callbacks

--- app/src/main/java/com/spacey/myhome/todo/TodoListRecyclerViewAdapter.kt
+++ app/src/main/java/com/spacey/myhome/todo/TodoListRecyclerViewAdapter.kt
@@ -3,21 +3,24 @@ package com.spacey.myhome.todo
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
 import com.example.data.todo.TodoEntity
 import com.spacey.myhome.databinding.FragmentTodoItemBinding
+import com.spacey.myhome.todo.todolist.TodoDiffCallback
 
 class TodoListRecyclerViewAdapter(
     initialTodoList: List<TodoEntity>
 ) : RecyclerView.Adapter<TodoListRecyclerViewAdapter.ViewHolder>() {
 
-    private var todoList: List<TodoEntity> = initialTodoList
+    private val todoList: MutableList<TodoEntity> = initialTodoList.toMutableList()
 
     fun updateTodoList(newList: List<TodoEntity>) {
-        if (todoList != newList) {
-            todoList = newList
-            notifyDataSetChanged()
-        }
+        val todoDiffCallback = TodoDiffCallback(todoList, newList)
+        val diffResult = DiffUtil.calculateDiff(todoDiffCallback)
+        todoList.clear()
+        todoList.addAll(newList)
+        diffResult.dispatchUpdatesTo(this)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
---
+++ app/src/main/java/com/spacey/myhome/todo/todolist/TodoDiffCallback.kt
@@ -0,0 +1,20 @@
+package com.spacey.myhome.todo.todolist
+
+import androidx.recyclerview.widget.DiffUtil
+import com.example.data.todo.TodoEntity
+
+class TodoDiffCallback(
+    private val oldTodoList: List<TodoEntity>,
+    private val newTodoList: List<TodoEntity>
+) : DiffUtil.Callback() {
+    override fun getOldListSize(): Int = oldTodoList.size
+
+    override fun getNewListSize(): Int = newTodoList.size
+
+    override fun areItemsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean =
+        oldTodoList[oldItemPosition].title == newTodoList[newItemPosition].title
+
+    override fun areContentsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean =
+        oldTodoList[oldItemPosition].isCompleted == newTodoList[newItemPosition].isCompleted
+
+}
