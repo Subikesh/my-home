b65b3decbaa0c648fe4da887387dd59d3d137c22
subikesh <subikeshps@gmail.com>
2023-06-05 23:40:42 +0530

    Implemented todo data fetch in repository

--- data/src/main/java/com/example/data/todo/TodoRepository.kt
+++ data/src/main/java/com/example/data/todo/TodoRepository.kt
@@ -1,7 +1,9 @@
 package com.example.data.todo
 
-import android.util.Log
 import com.example.data.ConnectivityHelper
+import com.example.data.todo.local.TodoDBEntity
+import com.example.data.todo.local.TodoLocalDataSource
+import com.example.data.todo.remote.TodoRemoteDataSource
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
@@ -9,26 +11,43 @@ import kotlinx.coroutines.flow.flowOn
 
 class TodoRepository(
     private val todoRemoteDataSource: TodoRemoteDataSource,
+    private val todoLocalDataSource: TodoLocalDataSource,
     private val connectivityHelper: ConnectivityHelper
 ) {
     suspend fun fetchTodos(): Flow<TodoResult> {
         return flow {
             emit(TodoResult.Loading)
-            // From local db
-//            emit(TodoLocalDataSource.getTodoList())
-
-            if (connectivityHelper.isInternetConnected()) {
-                try {
-                    val todoList = todoRemoteDataSource.getTodoList()
-                    Log.d("Todo", "Todo data: $todoList")
-                    emit(TodoResult.TodoData.Success(todoList))
-                } catch (e: Exception) {
-                    e.printStackTrace()
-                    emit(TodoResult.TodoData.Error(e))
+            try {
+                // From local db
+                val todoData = getTodosFromDB()
+                if (todoData is TodoResult.TodoData.Success) {
+                    emit(todoData)
                 }
+                emit(TodoResult.TodoData.Success(todoLocalDataSource.getAllTodos()))
+
+                if (connectivityHelper.isInternetConnected()) {
+                    val todoList = todoRemoteDataSource.getTodoList()
+                    todoLocalDataSource.insertAll(todoList.map { it.toDBEntity() })
+                    emit(TodoResult.TodoData.Success(todoLocalDataSource.getAllTodos()))
+                }
+            } catch (e: Exception) {
+                e.printStackTrace()
+                emit(TodoResult.TodoData.Error(e))
             }
 
         }.flowOn(Dispatchers.IO)
     }
+
+    private fun getTodosFromDB(): TodoResult =
+        todoLocalDataSource.getAllTodos().let {
+            if (it.isEmpty()) {
+                TodoResult.TodoData.Error(errorMessage = "No Todos found")
+            } else {
+                TodoResult.TodoData.Success(it)
+            }
+        }
+
+    private fun TodoEntity.toDBEntity(): TodoDBEntity =
+        TodoDBEntity(id, title, isCompleted, createdTime)
 
 }
