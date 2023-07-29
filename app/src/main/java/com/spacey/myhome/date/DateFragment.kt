07b8112f6a28e7c8708b6c1eb0aaa6b293604e61
Subikesh <subikeshps@gmail.com>
2023-07-29 20:45:42 +0530

    Loader added for date fields

--- app/src/main/java/com/spacey/myhome/date/DateFragment.kt
+++ app/src/main/java/com/spacey/myhome/date/DateFragment.kt
@@ -7,8 +7,9 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.LinearLayout
-import android.widget.Spinner
+import android.widget.ProgressBar
 import android.widget.TextView
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Lifecycle
@@ -16,15 +17,15 @@ import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.repeatOnLifecycle
 import com.spacey.myhome.R
 import com.spacey.myhome.view.CounterView
+import kotlinx.coroutines.flow.collectIndexed
 import kotlinx.coroutines.launch
 import java.util.Calendar
 
 class DateFragment : Fragment() {
 
-    private lateinit var typeSpinner: Spinner
     private lateinit var dateText: TextView
-
     private lateinit var fieldsContainer: LinearLayout
+    private lateinit var dateLoader: ProgressBar
 
     private val dateViewModel: DateViewModel by viewModels({ requireParentFragment() })
 
@@ -36,44 +37,39 @@ class DateFragment : Fragment() {
         // Inflate the layout for this fragment
         val rootView = inflater.inflate(R.layout.fragment_date, container, false)
         with(rootView) {
-            typeSpinner = findViewById(R.id.item_type)
             dateText = findViewById(R.id.date_text)
             fieldsContainer = findViewById(R.id.fields_layout)
+            dateLoader = findViewById(R.id.date_loader)
         }
         return rootView
     }
 
-
     @SuppressLint("SetTextI18n")
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        dateViewModel.getMetadata()
 
         viewLifecycleOwner.lifecycleScope.launch {
             viewLifecycleOwner.repeatOnLifecycle(Lifecycle.State.STARTED) {
-                dateViewModel.dateState.collect { dateState ->
+                dateViewModel.dateState.collectIndexed { index, dateState ->
                     fieldsContainer.removeAllViewsInLayout()
+                    setLoading(dateState is DateUIState.LOADING)
                     when (dateState) {
-                        is DateUIState.LOADING -> {
-                            val loadingText = TextView(requireContext())
-                            loadingText.text = "Loading current date"
-                            fieldsContainer.addView(loadingText)
-                        }
+                        is DateUIState.LOADING -> {}
                         is DateUIState.SUCCESS -> {
-                            for (field in dateState.fields) {
-                                val inflator = layoutInflater.inflate(R.layout.daily_field_view, fieldsContainer, false)
+                            for (expense in dateState.expenses) {
+                                val inflater = layoutInflater.inflate(R.layout.daily_field_view, fieldsContainer, false)
 
-                                with(inflator) {
+                                with(inflater) {
                                     val fieldText = findViewById<TextView>(R.id.field_text)
                                     val counterView = findViewById<CounterView>(R.id.field_counter)
-                                    fieldText.text = field.name
+                                    fieldText.text = expense.name
                                     counterView.setOnCountChangeListener { newCount ->
-                                        Log.d("Count", "Count changed for ${field.name} to $newCount")
+                                        Log.d("Count", "Count changed for ${expense.name} to $newCount")
                                     }
                                 }
-                                fieldsContainer.addView(inflator)
+                                fieldsContainer.addView(inflater)
                             }
-                            Log.d("Fields", "Success fields: ${dateState.fields}")
+                            Log.d("Fields", "Success fields: ${dateState.expenses}")
                         }
                         is DateUIState.ERROR -> {
                             val errorText = TextView(requireContext())
@@ -86,6 +82,11 @@ class DateFragment : Fragment() {
                 }
             }
         }
+    }
+
+    private fun setLoading(isLoading: Boolean) {
+        dateLoader.isVisible = isLoading
+        fieldsContainer.isVisible = !isLoading
     }
 
 }
--- app/src/main/java/com/spacey/myhome/date/DateViewModel.kt
+++ app/src/main/java/com/spacey/myhome/date/DateViewModel.kt
@@ -5,7 +5,7 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.spacey.myhome.data.DIServiceLocator
 import com.spacey.myhome.data.base.Data
-import com.spacey.myhome.data.expense.FieldEntity
+import com.spacey.myhome.data.expense.DateExpensesEntity
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
@@ -16,6 +16,9 @@ class DateViewModel : ViewModel() {
 
     private val _dateState: MutableStateFlow<DateUIState> = MutableStateFlow(DateUIState.LOADING(Calendar.getInstance()))
     val dateState: StateFlow<DateUIState> = _dateState
+
+    private val _metadataDownloaded = MutableStateFlow(false)
+    val metadataDownloaded: StateFlow<Boolean> = _metadataDownloaded
 
     private val expenseRepository = DIServiceLocator.expenseRepository
 
@@ -50,16 +53,17 @@ class DateViewModel : ViewModel() {
     }
 
     fun getMetadata() {
-//        viewModelScope.cancel()
+        _metadataDownloaded.value = false
         viewModelScope.launch {
             Log.d("MEtadata", DIServiceLocator.metadataRepository.getMetadata().toString())
+            _metadataDownloaded.value = true
         }
     }
-}
 
-private fun Data<List<FieldEntity>>.toUIState(date: Calendar): DateUIState {
-    return when (this) {
-        is Data.Success -> DateUIState.SUCCESS(date, this.data)
-        is Data.Error -> DateUIState.ERROR(date, this.exception)
+    private fun Data<DateExpensesEntity>.toUIState(date: Calendar): DateUIState {
+        return when (this) {
+            is Data.Success -> DateUIState.SUCCESS(date, this.data.expenses)
+            is Data.Error -> DateUIState.ERROR(date, this.exception)
+        }
     }
 }
