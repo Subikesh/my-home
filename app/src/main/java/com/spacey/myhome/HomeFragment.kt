3da03efdd8aeb2100d305233bc2ac175eb692298
Subikesh <subikeshps@gmail.com>
2023-07-29 20:41:20 +0530

    Metadata download with loading

--- app/src/main/java/com/spacey/myhome/HomeFragment.kt
+++ app/src/main/java/com/spacey/myhome/HomeFragment.kt
@@ -5,35 +5,40 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.CalendarView
+import android.widget.LinearLayout
+import android.widget.ProgressBar
 import android.widget.TextView
 import androidx.appcompat.widget.Toolbar
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import com.spacey.myhome.date.DateFragment
 import com.spacey.myhome.date.DateViewModel
+import kotlinx.coroutines.launch
 
 class HomeFragment : Fragment() {
 
+    private lateinit var homeLayout: LinearLayout
     private lateinit var homeCalendarView: CalendarView
     private lateinit var todayButton: TextView
+    private lateinit var homeProgress: ProgressBar
 
     private val dateViewModel: DateViewModel by viewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         val rootView = inflater.inflate(R.layout.fragment_home, container, false)
 
         with(rootView) {
+            homeLayout = findViewById(R.id.home_layout)
             homeCalendarView = findViewById(R.id.home_calendar)
             todayButton = findViewById(R.id.today_button)
+            homeProgress = findViewById(R.id.home_progress)
         }
-
-        val dateFragment = DateFragment()
-        childFragmentManager.beginTransaction()
-            .replace(R.id.date_fragment_container, dateFragment)
-            .commit()
 
         return rootView
     }
@@ -49,6 +54,27 @@ class HomeFragment : Fragment() {
     }
 
     private fun setListeners() {
+        lifecycleScope.launch {
+            viewLifecycleOwner.repeatOnLifecycle(Lifecycle.State.STARTED) {
+                dateViewModel.metadataDownloaded.collect { isDownloaded ->
+                    if (isDownloaded) {
+                        homeProgress.visibility = View.GONE
+                        homeLayout.visibility = View.VISIBLE
+
+                        val dateFragment = DateFragment()
+                        childFragmentManager.beginTransaction()
+                            .replace(R.id.date_fragment_container, dateFragment)
+                            .commit()
+
+                    } else {
+                        homeProgress.visibility = View.VISIBLE
+                        homeLayout.visibility = View.GONE
+                    }
+
+                }
+            }
+        }
+
         todayButton.setOnClickListener {
             val currentMillis = System.currentTimeMillis()
             homeCalendarView.setDate(currentMillis, true, true)
@@ -58,6 +84,8 @@ class HomeFragment : Fragment() {
         homeCalendarView.setOnDateChangeListener { _, year, month, dayOfMonth ->
             dateViewModel.setDate(year, month, dayOfMonth)
         }
+
+        dateViewModel.getMetadata()
     }
 
     companion object {
