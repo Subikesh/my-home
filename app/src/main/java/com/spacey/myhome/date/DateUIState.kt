d7315a7ff66fdbf28338050c6327bafe6c70d526
Subikesh <subikeshps@gmail.com>
2023-07-29 20:44:24 +0530

    DateUIState updated

--- app/src/main/java/com/spacey/myhome/date/DateUIState.kt
+++ app/src/main/java/com/spacey/myhome/date/DateUIState.kt
@@ -1,13 +1,13 @@
 package com.spacey.myhome.date
 
-import com.spacey.myhome.data.expense.FieldEntity
+import com.spacey.myhome.data.expense.ExpenseEntity
 import java.util.Calendar
 
 sealed class DateUIState(open val date: Calendar) {
 
     data class LOADING(override val date: Calendar) : DateUIState(date)
 
-    data class SUCCESS(override val date: Calendar, val fields: List<FieldEntity>) : DateUIState(date)
+    data class SUCCESS(override val date: Calendar, val expenses: List<ExpenseEntity>) : DateUIState(date)
 
     data class ERROR(override val date: Calendar, val error: Throwable?) : DateUIState(date)
 }
