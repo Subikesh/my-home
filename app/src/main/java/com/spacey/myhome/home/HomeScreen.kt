2ac0e724f69f0ff96c616495a952094d2c739226
subikesh.ps <subikesh.ps@zohocorp.com>
2023-10-23 10:12:19 +0530

    Added haptics to calendar and edit icon to date

--- app/src/main/java/com/spacey/myhome/home/HomeScreen.kt
+++ app/src/main/java/com/spacey/myhome/home/HomeScreen.kt
@@ -3,11 +3,16 @@ package com.spacey.myhome.home
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Edit
 import androidx.compose.material3.Button
 import androidx.compose.material3.DatePicker
 import androidx.compose.material3.DatePickerDialog
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -17,9 +22,13 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.hapticfeedback.HapticFeedbackType
+import androidx.compose.ui.platform.LocalHapticFeedback
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.spacey.myhome.utils.toLocalDate
 import java.time.LocalDate
 import java.time.format.TextStyle
@@ -38,23 +47,30 @@ fun HomeScreen() {
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 private fun HomeDatePicker(date: LocalDate, onDateChanged: (LocalDate) -> Unit) {
+    val haptics = LocalHapticFeedback.current
     var showDateDialog by remember { mutableStateOf(false) }
-    val textModifier = Modifier.clickable {
-        showDateDialog = true
+    Row(Modifier.clickable { showDateDialog = true }, verticalAlignment = Alignment.CenterVertically) {
+        Text(
+            "${date.dayOfMonth} ${date.month.getDisplayName(TextStyle.SHORT, Locale.getDefault())} ${date.year},\n${date.dayOfWeek}",
+            modifier = Modifier.weight(1f),
+            style = MaterialTheme.typography.displayMedium
+        )
+        Icon(Icons.Default.Edit, contentDescription = "Date edit", modifier = Modifier.padding(16.dp))
     }
-    Text(
-        "${date.dayOfMonth} ${date.month.getDisplayName(TextStyle.SHORT, Locale.getDefault())} ${date.year},\n${date.dayOfWeek}",
-        modifier = textModifier,
-        style = MaterialTheme.typography.displayMedium
-    )
     if (showDateDialog) {
         val dateState = rememberDatePickerState()
+
+        if (dateState.selectedDateMillis != null) {
+            haptics.performHapticFeedback(HapticFeedbackType.TextHandleMove)
+        }
+
         DatePickerDialog(
             onDismissRequest = { showDateDialog = false },
             confirmButton = {
                 Button(onClick = {
                     dateState.selectedDateMillis?.let { onDateChanged(it.toLocalDate()) }
                     showDateDialog = false
+                    haptics.performHapticFeedback(HapticFeedbackType.LongPress)
                 }) {
                     Text("Confirm")
                 }
