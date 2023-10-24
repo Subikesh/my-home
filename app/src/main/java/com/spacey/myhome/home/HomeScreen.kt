5e9a9658ac42b3c79098df88640e6c087ba58717
subikesh.ps <subikesh.ps@zohocorp.com>
2023-10-25 00:14:02 +0530

    Home screen with counter and checkbox field types

--- app/src/main/java/com/spacey/myhome/home/HomeScreen.kt
+++ app/src/main/java/com/spacey/myhome/home/HomeScreen.kt
@@ -3,31 +3,35 @@ package com.spacey.myhome.home
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.staggeredgrid.LazyVerticalStaggeredGrid
 import androidx.compose.foundation.lazy.staggeredgrid.StaggeredGridCells
 import androidx.compose.foundation.lazy.staggeredgrid.StaggeredGridItemSpan
-import androidx.compose.foundation.lazy.staggeredgrid.itemsIndexed
-import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.lazy.staggeredgrid.items
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Edit
+import androidx.compose.material.icons.filled.Remove
 import androidx.compose.material3.Button
-import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.DatePicker
 import androidx.compose.material3.DatePickerDialog
+import androidx.compose.material3.ElevatedCard
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.rememberDatePickerState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
@@ -36,6 +40,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.hapticfeedback.HapticFeedbackType
 import androidx.compose.ui.platform.LocalHapticFeedback
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -44,14 +49,18 @@ import java.time.LocalDate
 import java.time.format.TextStyle
 import java.util.Locale
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun HomeScreen() {
     var selectedDate: LocalDate by remember { mutableStateOf(LocalDate.now()) }
     val cardList = buildList {
-        val services = listOf("Milk","Water can","Gas","Maid")
-        for (i in 1..25) {
-            addAll(services.map { CardItem(it) })
+        val services = listOf(
+            Field.Counter("Milk", 1),
+            Field.Counter("Water can", 0),
+            Field.CheckBox("Gas"),
+//            Field.Amount("Maid")
+        )
+        for (i in 1..10) {
+            addAll(services)
         }
     }
     LazyVerticalStaggeredGrid(
@@ -67,19 +76,22 @@ fun HomeScreen() {
             }
         }
 
-        itemsIndexed(cardList) { i, card ->
-            Card(
-                onClick = { /*TODO*/ },
-                shape = RoundedCornerShape(20.dp),
-                elevation = CardDefaults.cardElevation(4.dp),
-                modifier = Modifier.size((((i % 3) + 1) * 100).dp)
-            ) {
-                Text(
-                    text = card.label,
-                    textAlign = TextAlign.Center,
-                    modifier = Modifier.fillMaxSize().padding(16.dp)
-                )
-            }
+        items(cardList) { field ->
+            field.CardView()
+//            Card(
+//                onClick = {  },
+//                shape = RoundedCornerShape(20.dp),
+//                elevation = CardDefaults.cardElevation(4.dp),
+//                modifier = Modifier.size((((i % 3) + 1) * 100).dp)
+//            ) {
+//                Text(
+//                    text = field.label,
+//                    textAlign = TextAlign.Center,
+//                    modifier = Modifier
+//                        .fillMaxSize()
+//                        .padding(16.dp)
+//                )
+//            }
         }
     }
 
@@ -133,12 +145,94 @@ private fun HomeDatePicker(date: LocalDate, onDateChanged: (LocalDate) -> Unit) 
     }
 }
 
-data class CardItem(val label: String)
+sealed class Field(open val label: String) {
+    var isVisible = mutableStateOf(true)
+
+    // TODO: Give enum class type as param instead of List<String>?
+    class Picklist(override val label: String, val options: List<String>, var value: String) :
+        Field(label)
+
+    class Date(override val label: String, var value: Long) : Field(label)
+    class Amount(override val label: String, var value: String = "") : Field(label)
+    class Counter(override val label: String, var value: Int = 0) : Field(label)
+    class CheckBox(override val label: String, var value: Boolean = false) : Field(label)
+
+    @OptIn(ExperimentalMaterial3Api::class)
+    @Composable
+    fun CardView(modifier: Modifier = Modifier) {
+        val haptic = LocalHapticFeedback.current
+        when (this) {
+            is Counter -> {
+                var count by remember { mutableIntStateOf(this.value) }
+                ElevatedCard(
+                    elevation = CardDefaults.elevatedCardElevation(4.dp),
+                    modifier = modifier
+                ) {
+                    Text(
+                        text = this@Field.label,
+                        style = MaterialTheme.typography.titleMedium,
+                        textAlign = TextAlign.Center,
+                        modifier = Modifier
+                            .padding(16.dp)
+                            .fillMaxWidth()
+                    )
+                    Row(
+                        verticalAlignment = Alignment.CenterVertically,
+                        modifier = Modifier.fillMaxWidth()
+                    ) {
+                        IconButton(onClick = {
+                            count--
+                            haptic.performHapticFeedback(HapticFeedbackType.TextHandleMove)
+                        }, enabled = count > 0, modifier = Modifier.weight(1f)) {
+                            Icon(Icons.Default.Remove, "Reduce count")
+                        }
+                        Text(
+                            text = count.toString(),
+                            style = MaterialTheme.typography.bodyMedium.copy(fontWeight = FontWeight.Bold)
+                        )
+                        IconButton(onClick = {
+                            count++
+                            haptic.performHapticFeedback(HapticFeedbackType.TextHandleMove)
+                        }, modifier = Modifier.weight(1f)) {
+                            Icon(Icons.Default.Add, "Increase count")
+                        }
+                    }
+                }
+            }
+
+            is CheckBox -> {
+                var checked by remember { mutableStateOf(false) }
+                // TODO: secondary color wont work here. Set it as green
+                val color =
+                    if (checked) MaterialTheme.colorScheme.secondaryContainer else MaterialTheme.colorScheme.errorContainer
+                ElevatedCard(
+                    colors = CardDefaults.cardColors(color),
+                    elevation = CardDefaults.elevatedCardElevation(4.dp),
+                    onClick = { checked = !checked }
+                ) {
+                    Text(
+                        text = this@Field.label,
+                        style = MaterialTheme.typography.titleMedium,
+                        textAlign = TextAlign.Center,
+                        modifier = Modifier
+                            .padding(16.dp)
+                            .fillMaxWidth()
+                    )
+                }
+            }
+
+            else -> {}
+        }
+    }
+}
 
 @Preview
 @Composable
 fun Preview() {
     Surface(Modifier.background(Color.White)) {
-        HomeScreen()
+        Column(Modifier.padding(16.dp)) {
+            Field.Counter("Sample", 10).CardView()
+            Field.CheckBox("True? False?").CardView()
+        }
     }
 }
