36fbe1b6193e97f207371b9def6c5e74e0564168
subikesh.ps <subikesh.ps@zohocorp.com>
2023-12-03 18:34:37 +0530

    Home FAB with done and add switching based on screens

--- app/src/main/java/com/spacey/myhome/MyHomeAppScreen.kt
+++ app/src/main/java/com/spacey/myhome/MyHomeAppScreen.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Done
 import androidx.compose.material3.Badge
 import androidx.compose.material3.BadgedBox
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -28,7 +29,9 @@ import androidx.compose.ui.hapticfeedback.HapticFeedbackType
 import androidx.compose.ui.platform.LocalHapticFeedback
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
 import androidx.navigation.NavHostController
+import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 import com.spacey.myhome.navigation.MyHomeNavHost
 import com.spacey.myhome.navigation.NavItem
@@ -109,12 +112,7 @@ fun MyHomeAppScreen(navController: NavHostController) {
                 }
             }
         }, floatingActionButton = {
-            LargeFloatingActionButton(onClick = {
-                navController.navigateTo(NavRoute.FORM)
-                haptics.performHapticFeedback(HapticFeedbackType.LongPress)
-            }) {
-                Icon(Icons.Default.Add, contentDescription = "Form")
-            }
+            HomeFAB(navController)
         }, contentWindowInsets = WindowInsets(16.dp, 8.dp, 16.dp, 8.dp)
     ) {
         Surface(Modifier.padding(it)) {
@@ -122,6 +120,32 @@ fun MyHomeAppScreen(navController: NavHostController) {
         }
     }
 }
+
+@Composable
+private fun HomeFAB(navController: NavController) {
+    val currentRoute = navController.currentBackStackEntryAsState()
+    val haptics = LocalHapticFeedback.current
+    when (currentRoute.value?.destination?.route) {
+        NavRoute.FORM.route -> {
+            // TODO: Handle form submit
+            LargeFloatingActionButton(onClick = {
+                haptics.performHapticFeedback(HapticFeedbackType.LongPress)
+                navController.popBackStack()
+            }) {
+                Icon(Icons.Default.Done, contentDescription = "Form submit done")
+            }
+        }
+
+        else -> {
+            LargeFloatingActionButton(onClick = {
+                haptics.performHapticFeedback(HapticFeedbackType.LongPress)
+                navController.navigateTo(NavRoute.FORM)
+            }) {
+                Icon(Icons.Default.Add, contentDescription = "Form")
+            }
+        }
+    }
+} 
 
 @Preview
 @Composable
