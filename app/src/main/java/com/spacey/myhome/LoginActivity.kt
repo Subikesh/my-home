ad8925960d41ff922535ef6aea6bc1c9465cd29a
Subikesh <subikeshps@gmail.com>
2024-08-17 19:52:24 +0530

    Initial commit & Login activity UI

---
+++ app/src/main/java/com/spacey/myhome/LoginActivity.kt
@@ -0,0 +1,33 @@
+package com.spacey.myhome
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.spacey.myhome.auth.AuthViewModel
+import com.spacey.myhome.databinding.ActivityMainBinding
+
+class LoginActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    private val authViewModel: AuthViewModel by viewModels()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+
+    }
+}
---
+++ app/src/main/java/com/spacey/myhome/auth/AuthViewModel.kt
@@ -0,0 +1,10 @@
+package com.spacey.myhome.auth
+
+import androidx.lifecycle.ViewModel
+
+class AuthViewModel : ViewModel() {
+
+    val isAuthenticated: Boolean
+        get() = true
+
+}
