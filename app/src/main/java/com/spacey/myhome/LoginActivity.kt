5d236bd0c7f3b5e75bde42235b832b81ba9dff29
Subikesh <subikeshps@gmail.com>
2024-08-20 08:23:39 +0530

    Fix for login screen keyboard scroll

--- app/src/main/java/com/spacey/myhome/LoginActivity.kt
+++ app/src/main/java/com/spacey/myhome/LoginActivity.kt
@@ -18,7 +18,6 @@ class LoginActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
